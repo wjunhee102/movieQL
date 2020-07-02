453fc96916869cb43357dfdc3d124f6e6ee1bf02
junhee <wjunhee102@gmail.com>
2020-07-02 18:08:59 +0900

    무비 API 연결

--- graphql/db.js
+++ graphql/db.js
@@ -1,44 +1,66 @@
-export const people = [
-  {
-    id: "0",
-    name: "Nicolas",
-    age : 18,
-    gender: "female"
-  },
-  {
-    id: "1",
-    name: "Nicolas",
-    age : 18,
-    gender: "female"
-  },
-  {
-    id: "2",
-    name: "Nicolas1",
-    age : 18,
-    gender: "female"
-  },
-  {
-    id: "3",
-    name: "Nicolas2",
-    age : 18,
-    gender: "female"
-  },
-  {
-    id: "4",
-    name: "Nicolas3",
-    age : 18,
-    gender: "female"
-  },
-  {
-    id: "5",
-    name: "Nicolas4",
-    age : 18,
-    gender: "female"
-  }
-];
+const API_URL = "https://yts.am/api/v2/list_movies.json";
 
 
-export const getById = id => {
-  const filteredPeople = people.filter(person => person.id === String(id))[0];
-  return filteredPeople
-}
+
+// let movies = [
+//   {
+//     id: 0,
+//     name: "Star Wars - The new one",
+//     score: 1
+//   },
+//   {
+//     id: 1,
+//     name: "Avangers - The new one",
+//     score: 8
+//   },
+//   {
+//     id: 2,
+//     name: "The Godfather I",
+//     score: 99
+//   },
+//   {
+//     id: 3,
+//     name: "Logan",
+//     score: 2
+//   },
+// ];
+
+// export const getMovies = () => movies;
+
+// const setMovies = newMovies => movies = newMovies;
+
+
+// export const getById = id => {
+//   const filteredMovies = movies.filter(movies => movies.id === id)[0];
+//   return filteredMovies
+// }
+
+// export const deleteMovie = id => {
+//   const cleanedMovies = movies.filter(movie => movie.id !== id);
+//   if (movies.length > cleanedMovies.length) {
+//     setMovies(cleanedMovies);
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// const makeId = (m) => {
+//   let newId = m[m.length-1].id + 1
+//   const idFilter = id => m.filter(ele => ele.id === id)[0]
+//   if(!idFilter(newId)) {
+//     return newId
+//   } else {
+//     return newId + 1
+//   }
+// }
+
+// export const addMovie = (name, score) => {
+//   const newMovie = {
+//     id: makeId(movies),
+//     name,
+//     score
+//   }
+//   movies.push(newMovie);
+//   return newMovie;
+// }
